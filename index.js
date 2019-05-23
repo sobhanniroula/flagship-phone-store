748ad42a281857ea9093c6434fa47225c00be640
sobhanniroula <sobhan_niroula@yahoo.com>
2019-05-23 14:44:06 +0300

    Testing commit: external css file not loaded in nodejs

--- index.js
+++ index.js
@@ -16,8 +16,28 @@ const server = http.createServer((req, res) => {
 } 
 
 else if (pathName === '/phone' && id < phoneData.length) {
-    res.writeHead(200, { 'Content-type': 'text/html'});
-    res.end(`This is the Phones page for phone ${id}!!`);
+    res.writeHead(200, { 'Content-type': 'text/html' });
+    res.writeHead(200, { 'Content-type': 'text/css' });
+
+    fs.readFile(`${__dirname}/templates/template-phones.html`, 'utf-8', (err, data) => {
+        const phone = phoneData[id];
+        let output = data.replace(/{%PRODUCTNAME%}/g, phone.productName);
+        output = output.replace(/{%IMAGE%}/g, phone.image);
+        output = output.replace(/{%DISPLAY%}/g, phone.display);
+        output = output.replace(/{%CAMERA%}/g, phone.camera);
+        output = output.replace(/{%MEMORY%}/g, phone.memory);
+        output = output.replace(/{%CPU%}/g, phone.cpu);
+        output = output.replace(/{%STORAGE%}/g, phone.storage);
+        output = output.replace(/{%BATTERY%}/g, phone.battery);
+        output = output.replace(/{%PRICE%}/g, phone.price);
+        output = output.replace(/{%DESCRIPTION%}/g, phone.description);
+        // using regular expressions above, because they might occur more than once in the code..
+        res.end(output);
+    });
+
+    var fileC = fs.readFile(`./css/style2.css`, 'utf-8');
+    res.write(fileC);
+    res.end();
 } 
 
 else {
